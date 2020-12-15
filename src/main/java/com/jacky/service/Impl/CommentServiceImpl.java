6b0e5d299efe6c7e2ebed400a5321cca6493008b
liujiang <2449767269@qq.com>
2020-12-15 14:26:37 +0800

    2020/12/15:博客评论功能

--- src/main/java/com/jacky/service/Impl/CommentServiceImpl.java
+++ src/main/java/com/jacky/service/Impl/CommentServiceImpl.java
@@ -3,11 +3,13 @@ package com.jacky.service.Impl;
 import com.jacky.dao.CommentRepository;
 import com.jacky.po.Comment;
 import com.jacky.service.CommentService;
+import org.springframework.beans.BeanUtils;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Sort;
 import org.springframework.stereotype.Service;
 
 import javax.transaction.Transactional;
+import java.util.ArrayList;
 import java.util.Date;
 import java.util.List;
 
@@ -23,7 +25,8 @@ public class CommentServiceImpl implements CommentService {
     @Override
     public List<Comment> listCommentByBlogId(Long blogId) {
         Sort sort = new Sort(Sort.Direction.DESC, "createTime");
-        return commentRepository.findByBlogId(blogId, sort);
+        List<Comment> comments = commentRepository.findByBlogIdAndParentCommentNull(blogId, sort);
+        return eachComment(comments);
     }
 
     @Transactional
@@ -37,5 +40,68 @@ public class CommentServiceImpl implements CommentService {
         }
         comment.setCreateTime(new Date());
         return commentRepository.save(comment);
+    }
+
+    /**
+     * 循环每个顶级的评论节点
+     *
+     * @param comments
+     * @return
+     */
+    private List<Comment> eachComment(List<Comment> comments) {
+        List<Comment> commentsView = new ArrayList<>();
+        for (Comment comment : comments) {
+            Comment c = new Comment();
+            BeanUtils.copyProperties(comment, c);
+            commentsView.add(c);
+        }
+        //合并评论的各层子代到第一级子代集合中
+        combineChildren(commentsView);
+        return commentsView;
+    }
+
+    /**
+     * 合并评论的各层子代到第一级子代集合
+     *
+     * @param comments
+     */
+    private void combineChildren(List<Comment> comments) {
+        for (Comment comment : comments) {
+            List<Comment> replys1 = comment.getReplyComments();
+            for (Comment reply1 : replys1) {
+                //循环迭代，找出子代，存放在tempReplys中
+                recursively(reply1);
+            }
+            //修改顶级节点的reply集合为迭代处理后的集合
+            comment.setReplyComments(tempReplys);
+            //清除临时存放区
+            tempReplys = new ArrayList<>();
+        }
+    }
+
+
+    /**
+     * 初始化临时存放区
+     */
+    private List<Comment> tempReplys = new ArrayList<>();
+
+    /**
+     * 递归迭代
+     *
+     * @param comment
+     */
+    private void recursively(Comment comment) {
+        //顶节点存放到临时存放区
+        tempReplys.add(comment);
+        if (comment.getReplyComments().size() > 0) {
+            List<Comment> replys = comment.getReplyComments();
+            for (Comment reply : replys) {
+                tempReplys.add(reply);
+                if (reply.getReplyComments().size() > 0) {
+                    //递归
+                    recursively(reply);
+                }
+            }
+        }
     }
 }
